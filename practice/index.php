132f959b6b57477d88882b26e7197c432de92edb
Michelle Soso <mis9235@g.harvard.edu>
2022-09-25 19:45:03 +0000

    traial code Bob Joe

--- practice/index.php
+++ practice/index.php
@@ -1,3 +1,9 @@
 <h1>Practice</h1>
 <img src="/images/fatty-corgi-Zn5chZcnFRA-unsplash.jpg" alt="Corgi-mourns" width="500" height="333">
 <h2>God Save the Queen</h2>
+
+<?php
+$var = 'Bob';
+$Var = 'Joe';
+echo "$var, $Var";      // outputs "Bob, Joe"
+?>
