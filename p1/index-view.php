4f83b62e709b0b72911cc7c40db3406308dfaf5d
Michelle Soso <mis9235@g.harvard.edu>
2022-09-26 03:06:30 +0000

    create index-view page and css page

---
+++ p1/index-view.php
@@ -0,0 +1,17 @@
+<!-- based on html template page I created in CSCI12 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>P1 Rock Paper Scissors Game by M Soso</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="styles/gamestyles.css">
+</head>
+<body>
+    <header>
+    </header>
+    <main>
+        <p> <a ></a></p>
+    </main>
+    <footer>    </footer>
+</body>
+</html>
