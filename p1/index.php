555c22427c402ac9d5221b9063465f575ad83c6a
Michelle Soso <mis9235@g.harvard.edu>
2022-09-28 14:13:58 +0000

    add html to index page

--- p1/index.php
+++ p1/index.php
@@ -1,4 +1,17 @@
-<h1>Project 1 Rock Paper Scissors</h1>
-
-
-
+<!-- based on html template page I created in CSCI12 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>P1 Rock Paper Scissors Game by M Soso</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="styles/gamestyles.css">
+</head>
+<body>
+    <header>
+    </header>
+    <main>
+        <p> <a ></a></p>
+    </main>
+    <footer>    </footer>
+</body>
+</html>
