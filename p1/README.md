375a35bf00e2300210877ed5c3719e1784205cf9
Michelle Soso <mis9235@g.harvard.edu>
2022-10-02 19:21:57 +0000

    project 1 work

--- p1/README.md
+++ p1/README.md
@@ -1,16 +1,18 @@
 Project 1- Rock Paper Scissors
 By Michelle Soso
 URL: http://www.hesgradwork.me/e2/p1/index.php
+
 Game planning
 Define variables of the game throws
 Define variables of the results
 Assign values to three throw options
 Randomly choose one of the throws from the array and assign it as Player A's move
 Randomly choose one of the throws from the array and assign it as Player B’s move
-Compare Player A move to Player B move 
+Compare Player A move to Player B move
 If Player A move is greater, assign value winner to Player A
 If Player A move is same as Player B move, assign value of draw to both players
 Otherwise, assign a value winner to Player B
-Report the results of Player A's Move, Player B's move, and the winner to the page 
+Report the results of Player A's Move, Player B's move, and the winner to the page
+Reset button for playing a new game
 Outside resources
-Pictures of game throw items a rock, sheet of paper and pair of scissors from unsplash.com 
+Pictures of game throw items a rock, sheet of paper and pair of scissors from unsplash.com
--- p1/index.php
+++ p1/index.php
@@ -1,22 +1,22 @@
 <?php
+// Create array of throws with three elements 
+$throw = ['rock', 'paper', 'scissors'];
 
-# <!-- based on html template page I created in CSCI12 -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>P1 Rock Paper Scissors Game by M Soso</title>
-    <meta charset="utf-8"/>
-    <link rel="stylesheet" href="styles/gamestyles.css">
-</head>
-<body>
-    <header>
-    </header>
-    <main>
-        <p> <a ></a></p>
-    </main>
-    <footer>    </footer>
-</body>
-</html>
+// Randomly choose one of the throws from the array and assign it as Player A's move
+$playerA = $throw[rand(0, 1, 2)];
+// Randomly choose one of the throws from the array and assign it as Player B’s move
+$playerB = $throw[rand(0, 1, 2)];
 
+var_dump($playerA);
+
+// Compare Player A move to Player B move with if then loop
+
+// If Player A move is greater, assign value winner to Player A
+// If Player A move is same as Player B move, assign value of draw to both players
+// Otherwise, assign a value winner to Player B
+// Report the results of Player A's Move, Player B's move, and the winner to the page 
+// Reset button for playing a new game
+
+require 'index-view.php';
 
 ?>
