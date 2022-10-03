b14c069eb5efe101b63106941a86f08d5b7ac770
Michelle Soso Micoso <mis9235@g.harvard.edu>
2022-10-03 14:40:03 +0000

    remove extra $ from echo statements

--- p1/index-view.php
+++ p1/index-view.php
@@ -12,11 +12,11 @@
     </header>
     <main>
         <p>
-       Player A threw $<?php echo $playerA; ?>  <br> 
-       Player B threw $<?php echo $playerB; ?>  <br>
-        $<?php echo $resultA; ?>  is the winner.
-       Player A threw $<?php echo $playerA; ?> <br>
-       Player B threw $<?php echo $playerB; ?> <br>
+       Player A threw <?php echo $playerA; ?>  <br> 
+       Player B threw <?php echo $playerB; ?>  <br>
+        <?php echo $resultA; ?>  is the winner.
+       Player A threw <?php echo $playerA; ?> <br>
+       Player B threw <?php echo $playerB; ?> <br>
        Game is a draw.
     </p>
     </main>
