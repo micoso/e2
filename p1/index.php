37be2a71ff72ccaf8fa1eceebcb65322a93f3cd6
Michelle Soso Micoso <mis9235@g.harvard.edu>
2022-10-03 14:14:39 +0000

    if elseif statements with no errors

--- p1/index.php
+++ p1/index.php
@@ -10,26 +10,24 @@ $playerA = $throw[rand(0, 1, 2)];
 // Randomly choose one throw from the array as Player B move
 $playerB = $throw[rand(0, 1, 2)];
 
-// Compare Player A throw to Player B throw with if elseif else for wins
-if ($playerA == 'rock' && $playerB == 'scissors'){
+// Compare Player A throw to Player B throw for draws and wins
+if ($playerA == $playerB) {
+($playerA && $playerB == 'Draw');
+}
+elseif ($playerA == 'rock' && $playerB == 'scissors'){
 ($playerA == 'Winner');
 }elseif ($playerA == 'paper' && $playerB == 'rock'){
 ($playerA == 'Winner');
-}else ($playerA == 'scissors') && ($playerB == 'paper'){
+}elseif($playerA == 'scissors' && $playerB == 'paper'){
 ($playerA == 'Winner');
-}
+}else ($playerB == 'Winner')
 
-// Compare Player A throw to Player B throw with if else for draws and B wins
-
-if ($playerA == $playerB) {
-($playerA == 'Draw' && $playerB == 'Draw');
-}
-else ($playerB == 'Winner')
 
 //var_dump($playerA);
 // var_dump($playerB);
 
 
 // Report the results of Player A's Move, Player B's move, and the winner to the page 
-// Reset button for playing a new game
+
+// Reset button for new game
 ?>
