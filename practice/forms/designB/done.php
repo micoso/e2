df9bb609f078626cc062783d0b3707a8f822453e
Michelle Soso Micoso <mis9235@g.harvard.edu>
2022-10-10 19:37:32 +0000

    changes to test form designB

---
+++ practice/forms/designB/done.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+$results = $_SESSION['results'];
+
+$haveAnswer = $results['haveAnswer'];
+$correct = $results['correct'];
+
+require 'done-view.php';
+
+
--- practice/forms/designB/process.php
+++ practice/forms/designB/process.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 $answer = $_POST['answer'];
 
 if ($answer == 'pumpkin') {
@@ -10,8 +12,14 @@ if ($answer == 'pumpkin') {
 
 
 # $answer = $_POST['answer'];
-
 # $correct = $answer == 'pumpkin';
+# require 'process-view.php';
 
-require 'process-view.php';
+$_SESSION['results'] = [
+    'haveAnswer' => $haveAnswer,
+    'correct' => $correct
+];
 
+//var_dump($_SESSION);
+
+header('Location: done.php');
