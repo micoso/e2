7045d2436d46bf28a7c8b39f9d1a450529ed98aa
Michelle Soso Micoso <mis9235@g.harvard.edu>
2022-10-10 23:08:49 +0000

    modify files to produce results as per form designC

---
+++ practice/forms/designC/process.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+
+$answer = $_POST['answer'];
+
+$haveAnswer = true;
+
+if($answer == '') {
+    $haveAnswer = false;
+} elseif ($answer == 'pumpkin') {
+    $correct = true;   
+} else {
+    $correct = false;  
+}
+
+$_SESSION['results'] = [
+    'haveAnswer' => $haveAnswer,
+    'correct' => $correct
+];
+
+//var_dump($_SESSION);
+
+
+header('Location:index.php');
