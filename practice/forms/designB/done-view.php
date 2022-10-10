0b80dee6fd4c7c9f9fae777454b015a9b04b6d81
Michelle Soso Micoso <mis9235@g.harvard.edu>
2022-10-10 19:35:52 +0000

    modify files for form DesignB

--- practice/forms/designB/done-view.php
+++ practice/forms/designB/done-view.php
@@ -5,14 +5,28 @@
     <meta charset='utf-8'>
     <link href=data: , rel=icon>
 </head>
+    <style>
+    .correct {
+        color: green;
+    }    
+
+    .incorrect {
+        color: red;
+    }
+    </style>
 <body>
 
 <h1>Your Results</h1>
 
-<?php if ($correct) { ?>
-    You are correct! :) 
+<?php if ($haveAnswer == false) { ?>
+Please enter an answer.
+<?php } elseif ($correct) { ?>
+<div class='correct'>
+    You are correct! 10 points to Hufflepuff! :)</div>
 <?php } else { ?>
-    Sorry incorrect :( <a href='index.php'>Try again.</a>
+<div class='incorrect'>
+    Sorry incorrect :(</div>
+<a href='index.php'>Try again.</a>
 <?php } ?>
     
 
