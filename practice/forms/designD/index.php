69a01b5370709a0d40564a2d4bd659e884d56c8b
Michelle Soso Micoso <mis9235@g.harvard.edu>
2022-10-12 01:10:10 +0000

    changes to practice string shuffle

--- practice/forms/designD/index.php
+++ practice/forms/designD/index.php
@@ -1,18 +1,5 @@
 <?php
 
-$words = [
-'evidence' => 'A discovery that helps solve a crime',
-'ponder' => 'To think carefully about something',
-'locate' => 'Discover the exact place or position of something or someone',
-'abridge' => 'to shorten by leaving out some parts',
-'regulate' => 'to make rules or laws that control something',
-'modest' => 'not overly proud or confident',
-'impromptu' => 'not prepared ahead of time',
-'stint' => 'a period of time spent at a particular activity',
-'tranquil' => 'free from disturbance or turmoil',
-'mutiny' => 'a turning of a group against a person in charge'
-];
-
 
 session_start();
 
---
+++ practice/strshuff.php
@@ -0,0 +1,29 @@
+<?php
+
+$words = [
+'evidence' => 'A discovery that helps solve a crime',
+'ponder' => 'To think carefully about something',
+'locate' => 'Discover the exact place or position of something or someone',
+'abridge' => 'to shorten by leaving out some parts',
+'regulate' => 'to make rules or laws that control something',
+'modest' => 'not overly proud or confident',
+'impromptu' => 'not prepared ahead of time',
+'stint' => 'a period of time spent at a particular activity',
+'tranquil' => 'free from disturbance or turmoil',
+'mutiny' => 'a turning of a group against a person in charge'
+];
+
+function shuffle_assoc($words) {
+  if (!is_array($words)) return $words;
+
+  $keys = array_keys($words);
+  shuffle($keys);
+  $random = array();
+  foreach ($keys as $key)
+    $random[$key] = $words[$key];
+
+  return $random; 
+
+  var_dump($random);
+
+  
