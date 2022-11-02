34d286c1742ed450abeb87bda7a78548742df413
Michelle Soso Micoso <mis9235@g.harvard.edu>
2022-11-02 03:18:51 +0000

    create initial files

---
+++ p2/index-view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+  <head>
+    <title>Project 2</title>
+    <meta charset="utf-8" />
+    <link href="data:," rel="icon" />
+  </head>
+
+  <body>
+    <h1>Rock Paper Scissors Version 2</h1>
+  </body>
+
+</html>
---
+++ p2/readme.md
@@ -0,0 +1,12 @@
+Project 2
+
+    By: Michelle Soso
+    URL: http://e2p2.hesgradwork.me
+
+Outside resources
+
+n/a
+
+Notes for instructor
+
+n/a
